8f7a01ea08d3203ce7d3412751424c11d342280b
Christoffer <261815@via.dk>
2017-12-16 22:19:14 +0100

    init

---
+++ app/src/main/java/memelord/com/barmaster/MainActivity.java
@@ -0,0 +1,27 @@
+package memelord.com.barmaster;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView oel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        oel = findViewById(R.id.imageOel);
+        oel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RecipeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
