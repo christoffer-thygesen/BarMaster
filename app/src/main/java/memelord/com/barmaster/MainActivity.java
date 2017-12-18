f41bc5b430198775f1dc422d9b6e70a8ff2012d4
Christoffer <stofferdude@hotmail.com>
2017-12-18 01:48:46 +0100

    .

--- app/src/main/java/memelord/com/barmaster/MainActivity.java
+++ app/src/main/java/memelord/com/barmaster/MainActivity.java
@@ -6,22 +6,49 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private ImageView drinks;
     private ImageView oel;
+    private ImageView varmDrikke;
+    private ImageView sjusser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        drinks = findViewById(R.id.imageDrinks);
         oel = findViewById(R.id.imageOel);
-        oel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, RecipeActivity.class);
-                startActivity(intent);
-            }
-        });
+        varmDrikke = findViewById(R.id.imageVarmeDrikke);
+        sjusser = findViewById(R.id.imageSjusser);
+
+        drinks.setOnClickListener(this);
+        oel.setOnClickListener(this);
+        varmDrikke.setOnClickListener(this);
+        sjusser.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(MainActivity.this, RecipeActivity.class);
+        switch (v.getId()) {
+            case R.id.imageDrinks:
+                intent.putExtra("categoryPicked", "Drinks");
+                break;
+
+            case R.id.imageOel:
+                intent.putExtra("categoryPicked", "Øl");
+                break;
+
+            case R.id.imageSjusser:
+                intent.putExtra("categoryPicked", "Sjusser");
+                break;
+
+            case R.id.imageVarmeDrikke:
+                intent.putExtra("categoryPicked", "Varme Drikke");
+                break;
+        }
+        startActivity(intent);
     }
 }
--- app/src/main/java/memelord/com/barmaster/RecipeActivity.java
+++ app/src/main/java/memelord/com/barmaster/RecipeActivity.java
@@ -2,12 +2,21 @@ package memelord.com.barmaster;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class RecipeActivity extends AppCompatActivity {
+
+    private TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+
+        Bundle categoryPicked = getIntent().getExtras();
+        String resultString = categoryPicked.getString("categoryPicked");
+
+        result = findViewById(R.id.resultText);
+        result.setText(resultString);
     }
 }
